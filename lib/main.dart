e87d4ee9f6705f8d1a1d6deeeb4377f0f7bc362e
Douglas A. Malcolm <dmalcolm@cpj.com>
2018-05-05 16:40:33 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = new WordPair.random(); // Add this line.
+    return new MaterialApp(
+      //title: 'Welcome to Flutter',
+      theme: new ThemeData(
+        primaryColor: Colors.blueAccent,
+      ),
+      home: new RandomWords(),
+    );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  // Add the next two lines.
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();   // Add this line.
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override                                  // Add from this line ...
+  Widget build(BuildContext context) {
+    //final WordPair wordPair = new WordPair.random();
+    //return new Text(wordPair.asPascalCase);
+
+    return new Scaffold (                   // Add from here...
+      appBar: new AppBar(
+        title: new Text('Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }                                          // ... to this line.
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(5.0),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row. For even rows, the function adds a ListTile row for
+        // the word pairing. For odd rows, the function adds a
+        // Divider widget to visually separate the entries. Note that
+        // the divider may be difficult to see on smaller devices.
+        itemBuilder: (BuildContext _context, int i) {
+          // Add a one-pixel-high divider widget before each row
+          // in the ListView.
+          if (i.isOdd) {
+            return new Divider();
+          }
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an
+          // integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings
+          // in the ListView,minus the divider widgets.
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the
+            // suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+       setState(() {
+         //In Flutter's reactive style framework, calling setState() triggers a call
+         // to the build() method for the State object, resulting in an
+         // update to the UI.
+         if(alreadySaved){
+           _saved.remove(pair);
+         }else{
+           _saved.add(pair);
+         }
+       });
+      },
+    );
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+          builder: (BuildContext context){
+            final Iterable<ListTile> titles = _saved.map(
+                (WordPair pair){
+                  return new ListTile(
+                    title: new Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                },
+            );
+           final List<Widget> divided = ListTile
+                .divideTiles(
+              context: context,
+             tiles: titles,
+            )
+            .toList();
+
+           return new Scaffold(
+             appBar: new AppBar(
+               title: const Text('Saved Favorites'),
+             ),
+             body: new ListView(children: divided),
+           );
+          }
+      )
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
